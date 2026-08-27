e27f55020f1a1e07754af28ee8c7f3ad963de9c7
Arundhati Singh <arundhatisingh171003@gmail.com>
2026-08-27 12:21:05 -0700

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        bool_arr = [False] * len(nums)
+        bool_arr[0] = True
+
+        for i in range(len(nums)):
+            if bool_arr[i] == True:
+                for j in range(1, nums[i] + 1):
+                    if i + j <= len(nums) - 1:
+                        bool_arr[i + j] = True
+
+        return bool_arr[-1]
